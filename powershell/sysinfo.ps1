d039a5fa31014d1d99cbec3d4d94da25c73d9caa
laprocea <laprocea@mail.uc.edu>
2022-09-09 19:13:27 -0400

    powershell files

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,7 +1,42 @@
-﻿$to = "elaprocina@gmail.com"
+﻿function getIP{
+    (Get-NetIPAddress).ipv4address | select-string "192*"
+}
+Write-Host(getIP)
+
+
+function getLocalUser{
+    (Get-LocalUser "*Administrator*").name
+}
+Write-output(getLocalUser)
+
+
+function getPCName{
+    hostname
+}
+Write-output(getPCName)
+
+
+function getVersion{
+    get-host | Select-Object version
+}
+Write-host(getVersion)
+
+
+function getDate{
+    Get-Date -format "dddd MM/dd/yyyy HH:mm"
+}
+write-host(getDate)
+
+
+$IP = getIP
+$USER = getLocalUser
+$PC = getPCName
+$VERSION = getVersion
+$DATE = getDate
+$to = "elaprocina@gmail.com"
 $from = "elaprocina@gmail.com"
 $subject = "IT3038c Windows SysInfo"
-$body = "this is a test"
+$body = "This machine's IP is $IP. User is $USER. Hostname is $PC. Powershell Version $VERSION. Today's date is $DATE."
 $smtp = "smtp.gmail.com"
 $port = "587"
 
