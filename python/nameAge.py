0dd51c8bc0a1ee5af5587b15444d1af00476f7f2
laprocea <laprocea@mail.uc.edu>
2022-09-29 22:10:28 -0400

    module work

--- python/nameAge.py
+++ python/nameAge.py
@@ -9,12 +9,37 @@ int(time.time() - start_time)
 print('What is your name?')
 myName = input()
 
+while myName != 'AJ':
+    if myName == 'your name':
+        print('Ha ha, very funny. Seriously, who are you?')
+        myName = input()
+
+    else:
+        print('That is not your name. Please, tell me your real name.')
+        myName = input()
+    
 print('Hello, ' + myName + '. That is a good name. How old are you?')
-myAge = input()
+myAge = int(input())    
 programAge = int(time.time() - start_time)
 
-print("%s? That's funny, I'm only %s seconds old." % (myAge, programAge))
-      
+if myAge < 13:
+    print("Learning young, that's good.")
+
+elif myAge == 13:
+    print("Your're a teenager now... that's cool, I guess")
+
+elif myAge > 13 and myAge < 30:
+    print("Still young, still learning...")
+
+elif myAge >= 30 and myAge < 65:
+    print("Now you're adulting")
+
+else:
+    print("...you've live a long time?")
+
+
+
+print("%s? That's funny, I'm only %s seconds old." % (myAge, programAge))      
 print('I wish i was ' + str(int(myAge) * 2) +  'years old')
 
 time.sleep(3)
